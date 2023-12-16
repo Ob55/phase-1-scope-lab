becbad7602c9f56bab3e37640f9549dd08e9ea29
Ob55 <brian55mwangi@gmail.com>
2023-12-16 21:32:34 +0300

    complite assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'
+  function upperCaseCustomerName(){
+     customerName =customerName.toUpperCase()
+     return customerName
+  }
+    function setBestCustomer(){
+       bestCustomer = 'not bob'
+    }
+    function overwriteBestCustomer(){
+      bestCustomer='maybe bob'
+      return overwriteBestCustomer
+    }
+     const leastFavoriteCustomer = 'brian'
+      function changeLeastFavoriteCustomer(newCustomer){
+          leastFavoriteCustomer = newCustomer
+      }
+        
+
+    
+    
